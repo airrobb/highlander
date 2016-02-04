ccb44d42c5cee14b662337c0e2abefc7e5607a7c
Aaron Robb <airrobb@gmail.com>
2016-02-04 21:04:37 +1300

    Unit tests

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -14,23 +14,30 @@ describe('immutability', () => {
       expect(state).to.equal(42)
     })
   })
-  describe('A List', () => {
+
+  describe('a tree', () => {
     function addMovie(currentState, movie) {
-      return currentState.push(movie)
+      return currentState.update('movies', movies => movies.push(movie))
     }
     it('is immutable', () => {
-      let state = List.of('Trainspotting', '28 Days Later')
+      let state = Map({
+        movies: List.of('Trainspotting', '28 Days Later')
+      })
       let nextState = addMovie(state, 'Sunshine')
 
-      expect(nextState).to.equal(List.of(
+      expect(nextState).to.equal(Map({
+        movies: List.of(
         'Trainspotting',
         '28 Days Later',
         'Sunshine'
-      ))
-      expect(state).to.equal(List.of(
+        )
+      }))
+      expect(state).to.equal(Map({
+        movies: List.of(
         'Trainspotting',
         '28 Days Later'
-      ))
+      )
+    }))
     })
   })
 })
